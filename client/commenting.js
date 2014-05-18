ac70cfcf8a5cc5456043235d9cbdc8c26ba2f6d8
Stuart P. Bentley <stuart@testtrack4.com>
2014-05-18 03:27:59 +0000

    Keep displayed messages sorted

--- client/commenting.js
+++ client/commenting.js
@@ -10,50 +10,50 @@ var secTime = 0;
 var roomId = 0;
 
 msgEntry.addEventListener('keypress', function(evt){
-if (evt.keyCode == 10 || evt.keyCode == 13) {
-  sendMessage({
-    body: msgEntry.value,
-    user: loggedInUser,
-    time: vidframe.currentTime,
-    date: Date.now()
-  });
-  msgEntry.value = "";
-}
+  if (evt.keyCode == 10 || evt.keyCode == 13) {
+    sendMessage({
+      body: msgEntry.value,
+      user: loggedInUser,
+      time: vidframe.currentTime,
+      date: Date.now()
+    });
+    msgEntry.value = "";
+  }
 });
 
 function ltInsertMessage(msg) {
-var i = 0;
-while (i < messages.length && (messages[i].time < msg.time ||
-    (messages[i].time == msg.time && messages[i].date <= msg.date))) {
-  i++;
-}
-
-messages.splice(i, 0, msg);
-if (msg.time <= vidframe.currentTime) {
-  var comment = ltCreateComment(msg);
-  if (i > 0) {
-    comments.insertBefore(comment, displayedCom[i-1]);
-  } else {
-    comments.appendChild(comment);
+  var i = 0;
+  while (i < messages.length && (messages[i].time < msg.time ||
+      (messages[i].time == msg.time && messages[i].date <= msg.date))) {
+    i++;
   }
-  displayedCom.push(comment);
-}
+
+  messages.splice(i, 0, msg);
+  if (msg.time <= vidframe.currentTime) {
+    var comment = ltCreateComment(msg);
+    if (i > 0) {
+      comments.insertBefore(comment, displayedCom[i-1]);
+    } else {
+      comments.appendChild(comment);
+    }
+    displayedCom.splice(i, 0, comment);
+  }
 }
 
 function ltUpdateTime(time) {
-secTime = time;
-while(displayedCom.length < messages.length && messages[displayedCom.length].time <= secTime){
-  var msg = ltCreateComment(messages[displayedCom.length]);
-  displayedCom.push(msg);
-  comments.insertBefore(msg, comments.firstChild);
-}
-while(displayedCom.length>0 && messages[displayedCom.length-1].time > secTime){
-  comments.removeChild(displayedCom.pop());
-}
+  secTime = time;
+  while(displayedCom.length < messages.length && messages[displayedCom.length].time <= secTime){
+    var msg = ltCreateComment(messages[displayedCom.length]);
+    displayedCom.push(msg);
+    comments.insertBefore(msg, comments.firstChild);
+  }
+  while(displayedCom.length>0 && messages[displayedCom.length-1].time > secTime){
+    comments.removeChild(displayedCom.pop());
+  }
 }
 
 vidframe.addEventListener('timeupdate',function(evt){
-ltUpdateTime(vidframe.currentTime);
+  ltUpdateTime(vidframe.currentTime);
 });
 
 function ltCreateComment(msg){
